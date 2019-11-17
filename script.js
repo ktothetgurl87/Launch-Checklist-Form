51b095e6bf2529315ba5d7df192b9c3cbbedd611
Katie Koski <KKoski@abstraktmg.com>
2019-11-17 15:11:44 -0600

    adding second function

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// Write your JavaScript code here!
+// Write your JavaScript code here
 
+window.addEventListener("submit", function(event){
+   let pilotNameField = document.getElementById("pilotName");
+   let copilotNameField = document.getElementById("copilotName");
+      if (pilotNameField || copilotNameField != ""){
+         alert("Make sure to enter valid information for each field!");
+         event.preventDefault();
+      }
+   });
+window.addEventListener("submit", function(event){
+   let fuelLevelField = document.getElementById("fuelLevel");
+   let cargoMassField = document.getElementById("cargoMass");
+   if (fuelLevelField.isNaN() || cargoMassField.isConnected.NaN() == false){
+      alert("Make sure to enter valid information for each field!")
+      event.preventDefault();
+   }
+});
+   
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
