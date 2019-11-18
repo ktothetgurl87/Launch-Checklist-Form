cd175a563e215b98b8ff29c1bdcf987e436b052f
Katie Koski <KKoski@abstraktmg.com>
2019-11-18 11:27:29 -0600

    added correct nan syntax

--- script.js
+++ script.js
@@ -3,20 +3,20 @@
 window.addEventListener("submit", function(event){
    let pilotNameField = document.getElementById("pilotName");
    let copilotNameField = document.getElementById("copilotName");
-      if (pilotNameField || copilotNameField != ""){
-         alert("Make sure to enter valid information for each field!");
-         event.preventDefault();
+      if (pilotNameField != "" || copilotNameField != ""){
+      alert("Make sure to enter valid information for each field!");
+      event.preventDefault();
       }
    });
 window.addEventListener("submit", function(event){
    let fuelLevelField = document.getElementById("fuelLevel");
    let cargoMassField = document.getElementById("cargoMass");
-   if (fuelLevelField.isNaN() || cargoMassField.isConnected.NaN() == false){
-      alert("Make sure to enter valid information for each field!")
-      event.preventDefault();
+   if (isNaN(fuelLevelField.value) || isNaN(cargoMassField.value)){
+   alert("Make sure to enter valid information for each field!");
+   event.preventDefault();
    }
 });
-   
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
